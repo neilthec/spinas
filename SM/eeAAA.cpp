58d724294a975b133646b7dfbbbe8ceb83ca32f5
Nick <majest11@msu.edu>
2026-09-20 16:52:47 -0500

    Checking slight reduced form of Feynman eeAAA.

--- SM/eeAAA.cpp
+++ SM/eeAAA.cpp
@@ -208,8 +208,9 @@ namespace spinas {
     if(ds3>0&&ds4>0&&ds5>0){
       
       return -sqrt(2)*sqrt(2)*sqrt(2)*e*e*e*(
-      a23a.v(ds2)*s525a.v()*s414a.v()*s13s.v(ds1) + a23a.v(ds2)*s525a.v()*s434a.v()*s13s.v(ds1)
-      + me*a23a.v(ds2)*s525a.v()*s34s.v()*a14a.v(ds1) - me*me*a23a.v(ds2)*s45s.v()*a45a.v()*s13s.v(ds1) 
+      // a23a.v(ds2)*s525a.v()*s414a.v()*s13s.v(ds1) + a23a.v(ds2)*s525a.v()*s434a.v()*s13s.v(ds1)
+      // + me*a23a.v(ds2)*s525a.v()*s34s.v()*a14a.v(ds1) 
+      - me*me*a23a.v(ds2)*s45s.v()*a45a.v()*s13s.v(ds1) 
       - me * a23a.v(ds2)*s45s.v()*s315a.v()*a14a.v(ds1) + me*s25s.v(ds2)*a35a.v()*s414a.v()*s13s.v(ds1) 
       + me*s25s.v(ds2)*a35a.v()*s434a.v()*s13s.v(ds1) + me*me*s25s.v(ds2)*a35a.v()*s34s.v()*a14a.v(ds1) 
       + me*s25s.v(ds2)*s423a.v()*a45a.v()*s13s.v(ds1) + me*s25s.v(ds2)*s453a.v()*a45a.v()*s13s.v(ds1) 
