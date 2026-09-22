ef505b19d2b44e8dae6d13519342bd3cb9c0c6ba
Nick <majest11@msu.edu>
2026-09-22 15:15:31 -0500

    #include <random> in eeAAA.h

--- SM/include/eeAAA.h
+++ SM/include/eeAAA.h
@@ -17,6 +17,10 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
+
+#include <random>
+
+
 namespace spinas {
 
   class eeAAA : public process {
